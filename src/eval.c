007ccb77498820f01506d2011e9d8c4193d4ba90
problue <1013644379@qq.com>
2017-08-01 22:49:09 +0800

    add eval procedure, scm_apply, and more

--- src/eval.c
+++ src/eval.c
@@ -6,25 +6,30 @@
 #include "error.h"
 #include "port.h"
 #include "print.h"
-#include "system.h"
+#include "scm.h"
 
+#define apply_primitive_procedure(proc, argc, argv) (((scm_primitive_proc *)proc)->prim(argc, argv))
+
+static scm_object* eval_prim(int, scm_object *[]);
 static scm_object* eval(scm_object *, scm_env *);
-static scm_object* apply_primitive_procedure(scm_object *, int, scm_object *[]);
 static scm_object* eval_definition(scm_object *, scm_env *);
-static scm_object* eval_assignment(scm_object *, scm_env *)
+static scm_object* eval_assignment(scm_object *, scm_env *);
 static scm_object* eval_lambda(scm_object *, scm_env *);
 static scm_env* make_apply_env(scm_compound_proc *, int, scm_object *[]);
 static int match_arity(scm_object *, int, scm_object *[]);
 
 static scm_env *global_env;
 
-jmp_buf eval_error_jmp;
+jmp_buf eval_error_jmp_buf;
 
 void scm_init()
 {
     global_env = scm_basic_env();
+}
 
-    scm_load_file("stdlib/lib.scm");
+void scm_init_eval(scm_env *env)
+{
+    scm_add_prim(env, "eval", eval_prim, 1, 2);
 }
 
 scm_object* scm_eval(scm_object *exp)
@@ -33,6 +38,30 @@ scm_object* scm_eval(scm_object *exp)
         return NULL;
     }
     return eval(exp, global_env);
+}
+
+scm_object* scm_apply(scm_object *proc, int argc, scm_object *argv[])
+{
+    if (SCM_PRIMPROCP(proc))
+        return apply_primitive_procedure(proc, argc, argv);
+    if (SCM_COMPROCP(proc)) {
+        if (match_arity(proc, argc, argv)) {
+            return eval(scm_make_begin(((scm_compound_proc *)proc)->body),
+                make_apply_env((scm_compound_proc *)proc, argc, argv));
+        }
+    }
+    return NULL;
+}
+
+static scm_object* eval_prim(int argc, scm_object *argv[])
+{
+    if (argc == 1) {
+        return eval(argv[0], global_env);
+    } else {
+        if (!SCM_NAMESPACEP(argv[1]))
+            return scm_wrong_contract("eval", "namespace?", 1, argc, argv);
+        return eval(argv[0], (scm_env *)argv[1]);
+    }
 }
 
 static scm_object* eval(scm_object *exp, scm_env *env)
@@ -48,14 +77,15 @@ static scm_object* eval(scm_object *exp, scm_env *env)
         case scm_void_type:
             return exp;
             
-        case scm_symbol_type:
-            scm_env_entry *entry = scm_env_lookup(env, (scm_symbol *)exp);
+        case scm_symbol_type: {
+            scm_env_entry *entry = scm_env_lookup(env, (scm_symbol *) exp);
             if (entry != NULL) {
                 return entry->val;
             } else {
-                return scm_undefined_identifier(entry->id);
+                return scm_undefined_identifier((scm_symbol *)exp);
             }
-            
+        }
+
         case scm_pair_type: {
             if (!SCM_LISTP(exp)) {
                 scm_print_error("application: bad syntax\n");
@@ -75,7 +105,7 @@ static scm_object* eval(scm_object *exp, scm_env *env)
                     return eval_lambda(exp, env);
                 if (SAME_OBJ(operator, scm_begin_symbol)) {
                     scm_object *exps = scm_begin_actions(exp);
-                    if(! SCM_NULLP(exps)) {
+                    if (! SCM_NULLP(exps)) {
                         for(; ! SCM_NULLP( SCM_CDR(exps) ); exps = SCM_CDR(exps) )
                             eval(SCM_CAR(exps), env);
                         exp = SCM_CAR(exps);
@@ -107,11 +137,12 @@ static scm_object* eval(scm_object *exp, scm_env *env)
             }
 
             if (SCM_PRIMPROCP(proc)) {
-                return apply_primitive_procedure(proc, argc, argv);
+                if (match_arity(proc, argc, argv))
+                    return apply_primitive_procedure(proc, argc, argv);
             } else if (SCM_COMPROCP(proc)) {
-                if(match_arity(proc, argc, argv)) {
-                    exp = scm_make_begin(((scm_compound_proc *) proc)->body);
-                    env = make_apply_env((scm_compound_proc *) proc, argc, argv);
+                if (match_arity(proc, argc, argv)) {
+                    exp = scm_make_begin(((scm_compound_proc *)proc)->body);
+                    env = make_apply_env((scm_compound_proc *)proc, argc, argv);
                     goto EVAL;
                 }
             } else {
@@ -126,12 +157,6 @@ static scm_object* eval(scm_object *exp, scm_env *env)
         default: ;
     }
     return NULL;
-}
-
-static scm_object* apply_primitive_procedure(scm_object *proc, int argc, scm_object *argv[])
-{
-    if(match_arity(proc, argc, argv))
-        return ((scm_primitive_proc *)proc)->prim(argc, argv);
 }
 
 static scm_object* eval_lambda(scm_object *exp, scm_env *env)
@@ -205,17 +230,17 @@ static scm_object* eval_assignment(scm_object *exp, scm_env *env)
     scm_symbol *id = scm_assignment_var(exp);
     scm_env_entry * entry = scm_env_lookup(env, id);
     if (entry != NULL) {
-        entry->val = eval(scm_assignment_var(exp), env);
+        entry->val = eval(scm_assignment_val(exp), env);
+    } else {
+        // if entry is NULL
+        scm_undefined_identifier(id);
     }
-    // if entry is NULL
-    scm_undefined_identifier(entry->id);
-    
     return scm_void;
 }
 
 static scm_env* make_apply_env(scm_compound_proc *proc, int argc, scm_object *argv[])
 {
-    if(proc->params_len > 0) {
+    if (proc->params_len > 0) {
         scm_env *apply_env = (scm_env *)scm_malloc_object(sizeof(scm_env));
         apply_env->rest = NULL;
 
