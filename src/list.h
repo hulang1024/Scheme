48e16e7d9dcc0100c6e60c3b14888c14630a4722
problue <1013644379@qq.com>
2017-08-04 22:23:17 +0800

    Add helper macro abstracts

--- src/list.h
+++ src/list.h
@@ -10,21 +10,25 @@
 #define SCM_LIST2(a, b) SCM_LCONS(a, SCM_LIST1(b))
 #define SCM_LIST3(a, b, c) SCM_LCONS(a, SCM_LIST2(b, c))
 #define SCM_LIST4(a, b, c, d) SCM_LCONS(a, SCM_LIST3(b, c, d))
+#define SCM_CAAR(o) SCM_CAR(SCM_CAR(o))
 #define SCM_CADR(o) SCM_CAR(SCM_CDR(o))
-#define SCM_CDAR(o) SCM_CDR(SCM_CAR(o))
 #define SCM_CDDR(o) SCM_CDR(SCM_CDR(o))
-#define SCM_CDDDR(o) SCM_CDR(SCM_CDDR(o))
-#define SCM_CADDR(o) SCM_CAR(SCM_CDDR(o))
+#define SCM_CDAR(o) SCM_CDR(SCM_CAR(o))
 #define SCM_CAADR(o) SCM_CAR(SCM_CADR(o))
+#define SCM_CADAR(o) SCM_CAR(SCM_CDAR(o))
+#define SCM_CADDR(o) SCM_CAR(SCM_CDDR(o))
+#define SCM_CDDDR(o) SCM_CDR(SCM_CDDR(o))
 #define SCM_CDADR(o) SCM_CDR(SCM_CADR(o))
 #define SCM_CADDDR(o) SCM_CAR(SCM_CDDDR(o))
 
+#define scm_list_for_each(list) \
+    for (; !SCM_NULLP(list); list = SCM_CDR(list))
 
 extern scm_object scm_null[];
 
 void scm_init_list(scm_env *env);
 scm_object* scm_make_pair(scm_object *, scm_object *);
-scm_object* scheme_make_list_pair(scm_object *, scm_object *);
+scm_object* scm_make_list_pair(scm_object *, scm_object *);
 scm_object* scm_build_list(int, scm_object **);
 int scm_list_length(scm_object *);
 int scm_is_list(scm_object *);
