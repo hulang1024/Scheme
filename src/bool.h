8c18ae0c2ec8ea06304995c85fe7f60c931fd264
problue <1013644379@qq.com>
2017-08-06 19:17:21 +0800

    Add: equals interfaces

--- src/bool.h
+++ src/bool.h
@@ -9,6 +9,10 @@ extern scm_object scm_true[];
 extern scm_object scm_false[];
 extern scm_object scm_void[];
 
+int scm_eq(scm_object *, scm_object *);
+int scm_eqv(scm_object *, scm_object *);
+int scm_equal(scm_object *, scm_object *);
+
 void scm_init_bool(scm_env *env);
 
 #endif //SCHEME_BOOL_H
